37845d21f5a6e6a0c45981be58dba4636af669ab
Theldus <davidsondfgl@gmail.com>
2020-01-23 14:17:57 -0300

    Feature: (Partial) ANSI Escape Sequences Support
    This commit adds partial support to some ANSI Escape Sequences,
    namely: (character 'd' corresponds to a number)
    
    - ESC [A (Cursor Upwards),
    - ESC [B (Cursor Downwards),
    - ESC [C (Cursor Forwards),
    - ESC [D (Cursor Backwards),
    - ESC [dJ (Erase Display),
    - ESC [dK (Erase in Line),
    - ESC [d;dH (Cursor Position),
    - ESC [?25l (DECTCEM: Hide Cursor),
    - ESC [?25h (DECTCEM: Show Cursor),
    and
    - ESC [d;dm (Select Graphics Rendition)

--- src/kernel/dev/tty/tty.h
+++ src/kernel/dev/tty/tty.h
@@ -1,18 +1,18 @@
 /*
  * Copyright(C) 2011-2016 Pedro H. Penna <pedrohenriquepenna@gmail.com>
- * 
+ *
  * This file is part of Nanvix.
- * 
+ *
  * Nanvix is free software; you can redistribute it and/or modify
  * it under the terms of the GNU General Public License as published by
  * the Free Software Foundation; either version 3 of the License, or
  * (at your option) any later version.
- * 
+ *
  * Nanvix is distributed in the hope that it will be useful,
  * but WITHOUT ANY WARRANTY; without even the implied warranty of
  * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
  * GNU General Public License for more details.
- * 
+ *
  * You should have received a copy of the GNU General Public License
  * along with Nanvix. If not, see <http://www.gnu.org/licenses/>.
  */
@@ -31,7 +31,7 @@
     #define GREEN         0x02
     #define CYAN          0x03
     #define RED           0x04
-    #define MAGNETA       0x05
+    #define MAGENTA       0x05
     #define BROWN         0x06
     #define LIGHT_GREY    0x07
     #define DARK_GREY     0x08
@@ -39,9 +39,12 @@
     #define LIGHT_GREEN   0x0A
     #define LIGHT_CYAN    0x0B
     #define LIGHT_RED     0x0C
-    #define LIGHT_MAGNETA 0x0D
+    #define LIGHT_MAGENTA 0x0D
     #define LIGHT_BROWN   0x0E
     #define WHITE         0x0F
+
+	/* Mode. */
+	#define COLOR_BOLD   0x800 /* Enable 'bold/bright' mode. */
 
 	/**
 	 * @brief TTY flags.
@@ -63,24 +66,24 @@
 		struct kbuffer rinput; /**< Cooked input buffer. */
 		struct kbuffer cinput; /**< Raw input buffer.    */
 	};
-	
+
 	/**
 	 * @name TTY Functions
 	 */
 	/**@{*/
 	EXTERN void tty_int(unsigned char);
 	/**@}*/
-	
+
 	/**
 	 * @name Console Functions
 	 */
 	/**@{*/
 	EXTERN void console_put(uint8_t, uint8_t);
 	EXTERN void console_init(void);
-	EXTERN void console_clear(void);
+	EXTERN void console_clear(int);
 	EXTERN void console_write(struct kbuffer *);
 	/**@}*/
-	
+
 	/**
 	 * @name Keyboard Functions
 	 */
