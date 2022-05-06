f2b622edea25c2e62f9dfbd3d4dcbb6e386e4d9a
Davidson Francis <davidsondfgl@gmail.com>
2022-05-06 01:51:06 -0300

    Remove pthread.h, as we dont support it

--- include/sys/features.h
+++ include/sys/features.h
@@ -155,7 +155,7 @@ extern "C" {
 #define _POSIX_THREAD_PROCESS_SHARED		200112L
 #define _POSIX_THREAD_SAFE_FUNCTIONS		200112L
 /* #define _POSIX_THREAD_SPORADIC_SERVER	    -1 */
-#define _POSIX_THREADS				200112L
+/* #define _POSIX_THREADS				200112L */
 /* #define _POSIX_TIMEOUTS			    -1 */
 #define _POSIX_TIMERS				     1
 /* #define _POSIX_TRACE				    -1 */
